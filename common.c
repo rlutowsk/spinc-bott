4950cdc22d525971a609ff7d19d4c43c2e751a47
Rafał Lutowski <rafal.lutowski@ug.edu.pl>
2024-07-14 23:42:15 +0200

    print log

--- common.c
+++ common.c
@@ -1,16 +1,27 @@
 #include "common.h"
 
-/* measurements */
-#include <time.h>
-
 struct timespec tv;
 void tic(void)
 {
     clock_gettime(CLOCK_REALTIME, &tv);
 }
-long toc(void)
+time_t toc(void)
 {
     static struct timespec st;
     clock_gettime(CLOCK_REALTIME, &st);
     return (tv.tv_sec==st.tv_sec) ? st.tv_nsec - tv.tv_nsec : 1000000000*(st.tv_sec-tv.tv_sec)+st.tv_nsec - tv.tv_nsec;
+}
+
+unsigned int verbosity_level = 0;
+
+void printlog(unsigned int v, const char *format, ...)
+{
+    va_list argptr;
+    if (v>verbosity_level) {
+        return;
+    }
+    printf("[%.6f] ", toc_sec());
+    va_start(argptr, format);
+    vfprintf(stdout, format, argptr);
+    va_end(argptr);
 }
--- common.h
+++ common.h
@@ -1,9 +1,24 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-void tic(void);
-long toc(void);
+#include <time.h>
+#include <stdio.h>
+
+void   tic(void);
+time_t toc(void);
+
+static inline float toc_sec(void)
+{
+    return toc()/1000000000.0;
+}
 
 void help(const char *name);
+
+extern unsigned int verbosity_level;
+
+//#define log(v, ...) printf("[%.6f] ", toc_sec()); if (v>=verbosity_level) {printf(__VA_ARGS__);}
+
+#include <stdarg.h>
+void printlog(unsigned int v, const char *format, ...);
 
 #endif
